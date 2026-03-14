d53ac0a51ac942486d3c39bdda2c4c79b18bea18
learonco <learonco@gmail.com>
2026-03-14 15:22:21 -0300

    Remove testing code

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from tools import geodesy
-
 def main():
     print("Hello from geodetic-advisor-ai-agent!")
-    # r = t.search_crs_objects.run({'bbox': {'north': -36.0523344, 'west': -71.9666079, 'south': -41.1006358, 'east': -67.9962632}, 'object_type': ['GEODETIC_REFERENCE_FRAME']})
-    r = geodesy.search_crs_objects.run({'object_type': ['GEODETIC_REFERENCE_FRAME'], 'object_area_of_use': 'Neuquen'})
-
-    print(r)
 
 
 if __name__ == "__main__":
